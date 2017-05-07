f19f6e297f4b23e6fa8fad7ba0462241f34870ea
hennnnes <hroemmer@googlemail.com>
2017-05-07 21:04:37 +0200

    added controls component

---
+++ frontend/src/Components/Controls/Controls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Controls = () => {
+    return (
+        <div>
+            <a href="" alt="controls">Kamera Controls</a>
+        </div>
+    )
+}
+
+export default Controls;
