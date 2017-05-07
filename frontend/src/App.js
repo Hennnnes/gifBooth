db7027654ec511dea45677986e5eea10de1293c8
hennnnes <hroemmer@googlemail.com>
2017-05-07 21:05:19 +0200

    customize sample app, remove unneeded logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './Components/Header/Header';
+import Preview from './Components/Preview/Preview';
+import Controls from './Components/Controls/Controls';
+import Footer from './Components/Footer/Footer';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+          <Header />
+          <Preview />
+          <Controls />
+          <Footer />
+
+          <div>
+              <h2>Some sample links to test stuff</h2>
+
+              <a href="http://localhost:8080/start">Turn on</a>
+              <a href="http://localhost:8080/stop">Turn on</a>
+
+              <a href="http://localhost:8080/stop">Create sample gif</a>
+          </div>
       </div>
     );
   }
