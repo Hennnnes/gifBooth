0613103fc0e743db733779a7357e4532f929e526
hennnnes <hroemmer@googlemail.com>
2017-05-30 13:28:49 +0200

    fetch json from server

--- frontend/src/Components/Controls/Controls.js
+++ frontend/src/Components/Controls/Controls.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './Controls.css';
 
-const Controls = () => {
-    return (
-        <div>
-            <a href="http://raspi11.local:8080/expose" alt="controls" className="btn">Create gif</a>
-        </div>
-    )
+class Controls extends React.Component {
+    constructor() {
+        super();
+
+        this.loadPath = this.loadPath.bind(this);
+
+        this.state = {
+            path: ''
+        }
+    }
+
+    loadPath() {
+        fetch('http://raspi11.local:8080/expose')
+        .then(response => {
+          if (!response.ok) {
+            throw Error(response.error);
+          }
+
+          return response.json();
+        })
+        .then(result => {
+            console.log(result.path);
+            this.setState({path: `http://raspi11.local/server/${result.path}`});
+        })
+        .catch(error => {
+          console.log('error');
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="btn" onClick={this.loadPath}>Create gif</button>
+
+                <img src={this.state.path} />
+            </div>
+        );
+    }
 }
 
 export default Controls;
