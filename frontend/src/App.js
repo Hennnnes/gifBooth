655bb920708a94b820f8c5cfc214466791344909
hennnnes <hroemmer@googlemail.com>
2017-06-13 15:15:50 +0200

    preview url from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,32 @@ class App extends Component {
   constructor() {
       super();
 
-
+      this.state = {
+          fetch_url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=waiting',
+          img_url: ''
+      }
   }
+
+  componentDidMount() {
+      fetch(this.state.fetch_url, { method: 'GET' })
+      .then(response => {
+          if (!response.ok) {
+              throw Error(response.error);
+          }
+
+          return response.json();
+      })
+      .then(result => {
+          this.setState({ img_url: result.data.image_url });
+      })
+  }
+
+
   render() {
     return (
       <div className="app">
         <Header />
-        <Preview />
+        <Preview url={this.state.img_url} />
         <Controls />
 
       </div>
