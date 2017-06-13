e780066a31212fc3b6e01ce4a5de0a317f83330d
hennnnes <hroemmer@googlemail.com>
2017-06-13 15:20:17 +0200

    preview as stateless function

--- frontend/src/Components/Preview/Preview.js
+++ frontend/src/Components/Preview/Preview.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import './Preview.css';
 
-class Preview extends React.Component {
-    render() {
-        return(
-            <div>
-                <img src={this.props.url} alt="some waiting gif" className="preview_image"/>
-            </div>
-        )
-    }
+
+const Preview = (props)  => {
+    return(
+        <div>
+            <img src={props.url} alt="some waiting gif" className="preview_image"/>
+        </div>
+    )
 }
 
 export default Preview;
