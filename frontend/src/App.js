00932dd4cadeaa5e11e669cad78c8f00677f45ff
hennnnes <hroemmer@googlemail.com>
2017-06-13 14:43:02 +0200

    load random gif preview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './Components/Header/Header';
+import Preview from './Components/Preview/Preview';
 import Controls from './Components/Controls/Controls';
 import Footer from './Components/Footer/Footer';
 
 class App extends Component {
+  constructor() {
+      super();
+
+
+  }
   render() {
     return (
       <div className="app">
         <Header />
+        <Preview />
         <Controls />
+
       </div>
     );
   }
--- frontend/src/Components/Preview/Preview.js
+++ frontend/src/Components/Preview/Preview.js
@@ -2,10 +2,45 @@ import React from 'react';
 
 import './Preview.css';
 
-const preview = () => {
-    return (
-        <div className="previewBox"></div>
-    )
+class Preview extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            url: 'http://placehold.it/300x300'
+        };
+
+        this.getGif = this.getGif.bind(this);
+    }
+
+    componentDidMount() {
+        this.getGif();
+    }
+
+    getGif() {
+        var url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=waiting';
+
+
+        fetch(url, { method: 'GET' })
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.error);
+            }
+
+            return response.json();
+        })
+        .then(result => {
+            this.setState({ url: result.data.image_url });
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <img src={this.state.url} alt="some waiting gif"/>
+            </div>
+        )
+    }
 }
 
-export default preview;
+export default Preview;
