553eab4837e59548c483db67066f5889be68f2b4
mia <mia.woelm@uptain.de>
2017-06-27 14:02:56 +0200

    centered stuff

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Header.css';
+
 const Header = () => {
     return (
         <h1>Gif booth</h1>
--- frontend/src/Components/Preview/Preview.js
+++ frontend/src/Components/Preview/Preview.js
@@ -8,7 +8,7 @@ const Preview = (props)  => {
     let downloadClassName = (props.customImage) ? 'download_button active' : 'download_button';
 
     return(
-        <div>
+        <div className="preview_wrapper">
           {props.visible ? <Loader /> :  <img src={props.url} alt="some waiting gif" className="preview_image"/>}
           {/* <img src={props.url} alt="some waiting gif" className="preview_image"/> */}
           <a href={props.url} download="test.gif" className={downloadClassName}>download</a>
